95b6e4519821f702090d980a8ef19a9a7545192d
Jan <solarer@hotmail.de>
2013-06-28 17:27:36 +0200

    neue Datei:   MD_Lennard_Fluid/main 	geändert:   MD_Lennard_Fluid/main.cpp

--- MD_Lennard_Fluid/main.cpp
+++ MD_Lennard_Fluid/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
 
 //#########################################################################
-int L=8;
-int N=16;
-int T=300;
-struct base
+int global_L=8;            //Kastenlänge
+int global_N=16;           //Anzahl der Teilchen
+int global_T=300;          //Temperatur
+double m=1.674927351e-27;            //Teilchenmasse in kg (Proton)
+const double kB=1.3806488e-23;         //Boltzmannkonstante
+struct base         //Structschablone für Ort oder Geschwindigkeit
 {
     double x,y;
 };
@@ -19,56 +22,86 @@ struct base
 vector<base> r;
 vector<base> v;
 
-
+//Init()
+//plaziert N Teilchen gleichmäßig im quadratischen Kasten mit Kantenlänge L,
+//gibt ihnen eine zufällige Geschwindigkeit zwischen -0.5 und +0.5 L pro Zeiteinheit
+//und zieht die Schwerpunktsgeschwindigkeit des Systems ab
 void init()
 {
-    double sqr=sqrtf(N);
-    double step= L/sqr; // Feld muss Platz für verschiebung bieten--> Teilchen von 0 und L liegen sonst aufeinander
-    for(int i=0; i*step<L; i++)
+    double sqr=sqrt(global_N);
+    double step= global_L/sqr; // Feld muss Platz für verschiebung bieten--> Teilchen von 0 und L liegen sonst aufeinander
+    for(int i=0; i*step<global_L; i++)
     {
-        base temp;
-        temp.x=i*step;
-        for(int k=0; k*step<L; k++)
+        base tempr;
+        tempr.x=i*step;
+        for(int k=0; k*step<global_L; k++)
         {
-            temp.y=k*step;
-            r.push_back(temp);
+            tempr.y=k*step;
+            r.push_back(tempr);
             base tempv;
-            tempv.x= 1.0*rand()/RAND_MAX*L-L*0.5;
-            tempv.y= 1.0*rand()/RAND_MAX*L-L*0.5;
+            tempv.x= 1.0*rand()/RAND_MAX*global_L-global_L*0.5; //Zufallsgeschwindigkeit zwischen -0.5L und +0.5L
+            tempv.y= 1.0*rand()/RAND_MAX*global_L-global_L*0.5; //Zufallsgeschwindigkeit zwischen -0.5L und +0.5L
             v.push_back(tempv);
+
         }
 
     }
 
-    base sum={0,0};
-    for(int i=0;i<N;i++)
+    base sum= {0,0};
+    for(int i=0; i<global_N; i++) //ermittele Durchschnittsgeschwindigkeit
     {
-        sum.x+=v[i].x/N;
-        sum.y+=v[i].y/N;
+        sum.x+=v[i].x/global_N;
+        sum.y+=v[i].y/global_N;
 
     }
-    for(int i=0;i<N;i++)
+    for(int i=0; i<global_N; i++) //ziehe Durchschnittsgeschwindigkeit ab
     {
         v[i].x-=sum.x;
         v[i].y-=sum.y;
 
     }
+
+
     sum.x=0;
     sum.y=0;
-    for(int i=0;i<N;i++)
+    for(int i=0; i<global_N; i++) //Testausgabe
     {
-        sum.x+=v[i].x/N;
-        sum.y+=v[i].y/N;
+        sum.x+=v[i].x/global_N;
+        sum.y+=v[i].y/global_N;
 
     }
     printf("Vsx=\t%f\nVsy=\t%f\n",sum.x,sum.y);
 
 }
 
+//gibt aktuelle Temperatur des Systems zurück in Kelvin
+double gettemp() {
+    base sum= {0,0};
+    for(int i=0 ; i<global_N ; i++)
+    {
+        sum.x += abs(v[i].x);
+        sum.y += abs(v[i].y);
+    }
+    return 0.5*m/kB*((sum.x*sum.x)+(sum.y*sum.y))/global_N;
+}
+
+//ändert Teilchen Geschwindigkeiten so, dass E=kB*T
+void settemp(float T){
+    double T_akt=gettemp();
+    for(int i=0 ; i<global_N ; i++)
+    {
+        v[i].x/=sqrt(T_akt/T);
+        v[i].y/=sqrt(T_akt/T);
+    }
+
+}
+
 int main()
 {
-
+    srand(time(NULL));
     init();
-    cout << "Hello world!"<<sizeof(long double)<< endl;
+    cout <<gettemp()<<endl;
+    settemp(global_T);
+    cout << gettemp();
     return 0;
 }
